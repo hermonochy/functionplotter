eb500f8d9086fbd9eebd3ef663d9393f274c22f9
fcrt <you@example.com>
2023-12-28 17:26:21 +0000

    cleanup

--- guiplot.py
+++ guiplot.py
@@ -4,22 +4,14 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import PySimpleGUI as sg
 import matplotlib
 import matplotlib.pyplot as plt
-
 from py_expression.core import Exp
+matplotlib.use("TkAgg")
+
 exp = Exp()
 
-
-#fig = Figure(figsize=(5, 4), dpi=100)
 fig, ax = plt.subplots()
 t = np.arange(0, 3, .01)
-
 line2 = ax.plot(t, 2 * np.sin(2 * np.pi * t))[0]
-#ax.set(xlim=[0, 3], ylim=[-4, 10])
-
-
-
-#fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
-matplotlib.use("TkAgg")
 
 def draw_figure(canvas, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
@@ -27,23 +19,19 @@ def draw_figure(canvas, figure):
     figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=1)
     return figure_canvas_agg
 
-def update_figure(fg, canvas, figure):
-    #figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+def update_figure(fg,):
     fg.draw()
-    #figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=1)
-    #return figure_canvas_agg
-
     
 layout = [
     [sg.Text("Plot test")],
     [sg.Canvas(key="-CANVAS-")],
-    [sg.Slider( orientation = "horizontal",key = "defstart",range = (-100,0),default_value = -3), sg.Slider( orientation = "horizontal",key = "defend",range = (0,100),default_value = 3) ],    
-    [sg.In("sin (x)")],    
+    [sg.Slider( orientation = "horizontal",key = "defstart",range = (-100,0),default_value = -3), 
+       sg.Slider( orientation = "horizontal",key = "defend",range = (0,100),default_value = 3) ],    
+    [sg.In("sin (x)", key='expression')],    
     [sg.Button("Plot")],
-    [sg.Button("Ok")],    
+    [sg.Button("Quit")],    
 ]
 
-# Create the form and show it without the plot
 window = sg.Window(
     "Matplotlib Single Graph",
     layout,
@@ -57,32 +45,26 @@ figureCanvas = draw_figure(window["-CANVAS-"].TKCanvas, fig)
 
 while True:
     event, values = window.read()
-    # End program if user closes window or
-    # presses the OK button
-    print ("daddy is dumb",event,values)
-    if event == "OK" or event == sg.WIN_CLOSED:
+    print ("event loop info: ",event,values)
+    if event == "Quit" or event == sg.WIN_CLOSED:
         break
 
     if event == "Plot":
-        print ("plot button", values[0])       
-        operand =exp.parse(values[0])
+        print ("plot button", values['expression'])       
+        operand =exp.parse(values['expression'])
         
         ax.set(xlim=[values["defstart"], values["defend"]], ylim=[-4, 10])
         
         xvals =np.linspace(values["defstart"],values["defend"],500 )
-        
-        
-        
         yvals = []
+        
         try:
           for x in xvals:
               yvals.append(operand.eval({"x":x}))
-          # Add the plot to the window
           line2.set_xdata(xvals)
           line2.set_ydata(yvals)
-          update_figure(figureCanvas, window["-CANVAS-"].TKCanvas, fig)
-
-        except (KeyError, TypeError,ValueError):
-          print("formel error")     
+          update_figure(figureCanvas)
+        except (KeyError, TypeError,ValueError) as e:
+          sg.popup("Formula cannot be evaluated. " + str(e))     
         
 window.close()
